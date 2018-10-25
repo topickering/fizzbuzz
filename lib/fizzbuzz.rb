82efd9b31943e3368145e90d5f0bac07f512a76a
topickering <topickering@gmail.com>
2018-10-25 12:50:37 +0000

    Program and tests passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number%3 == 0 && number%5 == 0 
+  puts 'fizzbuzz' 
+  elsif number%3 == 0 
+  puts 'fizz' 
+  elsif number%5 == 0 
+  puts 'buzz'
+  else
+  puts number
+  end
 end
